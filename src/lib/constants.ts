631f2033daab996517dd3d59e7588641d737a5fa
Aren <71951258+Aren1414@users.noreply.github.com>
2025-06-10 17:48:29 +0330

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
+// App Metadata
 export const APP_URL = process.env.NEXT_PUBLIC_URL!;
 export const APP_NAME = process.env.NEXT_PUBLIC_FRAME_NAME;
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_FRAME_DESCRIPTION;
@@ -11,3 +12,15 @@ export const APP_BUTTON_TEXT = process.env.NEXT_PUBLIC_FRAME_BUTTON_TEXT;
 export const APP_WEBHOOK_URL = process.env.NEYNAR_API_KEY && process.env.NEYNAR_CLIENT_ID 
     ? `https://api.neynar.com/f/app/${process.env.NEYNAR_CLIENT_ID}/event`
     : `${APP_URL}/api/webhook`;
+
+// Smart Contract Addresses
+export const WELCOME_NFT_ADDRESS = "0x40649af9dEE8bDB94Dc21BA2175AE8f5181f14AE";
+export const MONPORT_FACTORY_ADDRESS = "0x3FFA88641129f4Ac39287Bd2768A165Ac6055e31";
+
+// Import ABIs
+import WelcomeNFT_ABI from "./WelcomeNFT.json";
+import MonPortFactory_ABI from "./MonPortFactory.json";
+
+// Export ABIs
+export const WELCOME_NFT_ABI = WelcomeNFT_ABI;
+export const MONPORT_FACTORY_ABI = MonPortFactory_ABI;
