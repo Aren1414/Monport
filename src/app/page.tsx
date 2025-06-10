3b943fe494e737a088244570cd6b4711ced02721
Aren <71951258+Aren1414@users.noreply.github.com>
2025-06-10 20:51:44 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,12 @@
 "use client";
 
-import { Metadata } from "next";
 import { useRouter } from "next/navigation";
 import WelcomeTab from "~/components/WelcomeTab";
 import SwapTab from "~/components/SwapTab";
 import DeployTab from "~/components/DeployTab";
 import ProfileTab from "~/components/ProfileTab";
 import LeaderboardTab from "~/components/LeaderboardTab";
-import { APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "~/lib/constants";
-import { getFrameEmbedMetadata } from "~/lib/utils";
 import "~/styles/App.css";
-
-export const revalidate = 300;
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: APP_NAME,
-    openGraph: {
-      title: APP_NAME,
-      description: APP_DESCRIPTION,
-      images: [APP_OG_IMAGE_URL],
-    },
-    other: {
-      "fc:frame": JSON.stringify(getFrameEmbedMetadata()),
-    },
-  };
-}
 
 export default function Home() {
   const router = useRouter();
