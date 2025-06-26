86196b81084a3a131e829be759c16605fcf3738f
Aren <71951258+Aren1414@users.noreply.github.com>
2025-06-27 01:24:16 +0330

    Update page.tsx

--- src/app/share/[fid]/page.tsx
+++ src/app/share/[fid]/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { APP_URL, APP_NAME, APP_DESCRIPTION } from "~/lib/constants";
+import { getFrameEmbedMetadata } from "~/lib/utils";
 
 export const revalidate = 300;
 
@@ -12,33 +13,15 @@ export async function generateMetadata({
   const { fid } = params;
   const imageUrl = `${APP_URL}/api/opengraph-image?fid=${fid}`;
 
-  const frame = {
-    version: "next",
-    imageUrl,
-    button: {
-      title: "Open MonPort",
-      action: {
-        type: "launch_frame",
-        url: `${APP_URL}/?tab=welcome`,
-        name: APP_NAME,
-        splashImageUrl: `${APP_URL}/logo.png`,
-        splashBackgroundColor: "#ffffff",
-      },
-    },
-  };
-
   return {
     title: `${APP_NAME} - Share`,
-    description: APP_DESCRIPTION,
     openGraph: {
       title: APP_NAME,
       description: APP_DESCRIPTION,
       images: [imageUrl],
-      url: `${APP_URL}/share/${fid}`,
-      type: "website",
     },
     other: {
-      "fc:frame": JSON.stringify(frame),
+      "fc:frame": JSON.stringify(getFrameEmbedMetadata(imageUrl)),
     },
   };
 }
