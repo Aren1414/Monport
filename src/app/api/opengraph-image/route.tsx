579739589b9e11b8f1bd00ff9ecaf1c5631dca13
Aren <71951258+Aren1414@users.noreply.github.com>
2025-06-28 14:42:04 +0330

    Update route.tsx

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -1,22 +1,42 @@
 import { ImageResponse } from "next/og";
-import { NextRequest } from "next/server";
 
+// export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
-export async function GET(_request: NextRequest) {
+export async function GET() {
+  const imageUrl =
+    "https://gv5vt5azz4e7rtxhmmhtrzqnx5q3f7yockked6bazjvmrr5rqgta.arweave.net/NXtZ9BnPCfjO52MPOOYNv2Gy_w4SlEH4IMpqyMexgaY";
+
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full items-center justify-center bg-black">
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "black",
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://gv5vt5azz4e7rtxhmmhtrzqnx5q3f7yockked6bazjvmrr5rqgta.arweave.net/NXtZ9BnPCfjO52MPOOYNv2Gy_w4SlEH4IMpqyMexgaY"
+          src={imageUrl}
           alt="Preview"
-          tw="w-full h-full object-cover"
+          width={1024}
+          height={1024}
+          style={{
+            width: "1024px",
+            height: "1024px",
+            objectFit: "cover",
+            borderRadius: "32px",
+          }}
         />
       </div>
     ),
     {
       width: 1200,
-      height: 630,
+      height: 800,
     }
   );
 }
