6de6612a12b7e3bc67d4aeaf8d45f1f0f47c6fa3
Aren <71951258+Aren1414@users.noreply.github.com>
2025-06-29 01:45:10 +0330

    Update WelcomeTab.tsx

--- src/tabs/WelcomeTab.tsx
+++ src/tabs/WelcomeTab.tsx
@@ -11,8 +11,6 @@ const WELCOME_CONTRACT_ADDRESS = '0x40649af9dEE8bDB94Dc21BA2175AE8f5181f14AE'
 const NFT_PRICE = 0.3
 const VIDEO_URL =
   'https://zxmqva22v53mlvaeypxc7nyw7ucxf5dat53l2ngf2umq6kiftn3a.arweave.net/zdkKg1qvdsXUBMPuL7cW_QVy9GCfdr00xdUZDykFm3Y'
-const IMAGE_PREVIEW_URL =
-  'https://mhz5j3u6p57zkqjpwsz5kixyp37p7vkwqoetwxbtzgjof6fyysgq.arweave.net/YfPU7p5_f5VBL7Sz1SL4fv7_1VaDiTtcM8mS4vi4xI0'
 const MINI_APP_URL = 'https://monport-three.vercel.app/?tab=welcome'
 
 const MONAD_TESTNET_CHAIN_ID = 10143
