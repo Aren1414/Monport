dfaa150735f204597a011a728b3f65fa9a09309a
Aren <71951258+Aren1414@users.noreply.github.com>
2025-06-28 18:06:32 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,11 @@ import {
   APP_NAME,
   APP_DESCRIPTION,
   APP_OG_IMAGE_URL,
+  APP_BUTTON_TEXT,
+  APP_URL,
+  APP_SPLASH_URL,
+  APP_SPLASH_BACKGROUND_COLOR,
 } from "~/lib/constants";
-import { getFlatFrameMetadata } from "~/lib/utils";
 
 export const metadata: Metadata = {
   title: APP_NAME,
@@ -17,9 +20,6 @@ export const metadata: Metadata = {
     title: APP_NAME,
     description: APP_DESCRIPTION,
     images: [APP_OG_IMAGE_URL],
-  },
-  other: {
-    ...getFlatFrameMetadata(), 
   },
 };
 
@@ -30,6 +30,21 @@ export default async function RootLayout({
 }>) {
   const session = await getSession();
 
+  const frameMeta = JSON.stringify({
+    version: "next",
+    imageUrl: APP_OG_IMAGE_URL,
+    button: {
+      title: APP_BUTTON_TEXT ?? "Open Monport",
+      action: {
+        type: "launch_frame",
+        url: `${APP_URL}/?tab=welcome`,
+        name: APP_NAME,
+        splashImageUrl: APP_SPLASH_URL,
+        splashBackgroundColor: APP_SPLASH_BACKGROUND_COLOR ?? "#ffffff",
+      },
+    },
+  });
+
   return (
     <html lang="en">
       <head>
@@ -38,6 +53,8 @@ export default async function RootLayout({
         <meta name="description" content={APP_DESCRIPTION} />
         <meta name="theme-color" content="#2266ee" />
         <title>{APP_NAME}</title>
+
+        <meta property="fc:frame" content={frameMeta} />
 
         <Script
           src="https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.umd.min.js"
