f762279a2d38e23dd74983dd8f96fdbf17f96524
Aren <71951258+Aren1414@users.noreply.github.com>
2025-07-01 03:56:51 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const nextConfig: NextConfig = {
       ...config.resolve,
       alias: {
         ...(config.resolve?.alias || {}),
-        "~": path.resolve(__dirname, "src")
+        "~": path.resolve(__dirname, "src"),
+        "@": path.resolve(__dirname, "src")
       }
     };
     return config;
