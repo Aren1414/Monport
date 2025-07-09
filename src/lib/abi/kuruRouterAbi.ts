dafd647a2efd27face667fbbddc7235a7715711d
Aren <71951258+Aren1414@users.noreply.github.com>
2025-07-09 15:24:51 +0330

    Create kuruRouterAbi.ts

---
+++ src/lib/abi/kuruRouterAbi.ts
@@ -0,0 +1,16 @@
+export const KURU_ROUTER_ABI = [
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "amountIn", "type": "uint256" },
+      { "internalType": "uint256", "name": "minAmountOut", "type": "uint256" },
+      { "internalType": "address[]", "name": "path", "type": "address[]" },
+      { "internalType": "address[]", "name": "pools", "type": "address[]" },
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "uint256", "name": "deadline", "type": "uint256" }
+    ],
+    "name": "swapExactTokensForTokens",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
